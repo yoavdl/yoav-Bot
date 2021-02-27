1e3824039b1018881d3d072bb327f94e88b942b0
YoavDL <79748439+yoavdl@users.noreply.github.com>
2021-02-27 03:48:50 -0800

    Yoav bot

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log('yoav Bot is online!');
+});
+
+
+
+client.login('ODE1MTMzOTQ5Njk3MDY0OTgw.YDn-pg.89h8KXFP1D45dxrXlyfLHOtip-o');
